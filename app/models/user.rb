2d433ee2b1f2427817de10b20828a4c406f9cb23
Ryan Resella <ryan@codeforamerica.org>
2011-10-31 16:54:56 -0700

    Setup initial LinkedIn config

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ActiveRecord::Base
       user.name = auth["user_info"]["name"]
     end
   end
+
+  protected
+
+  def linked_in_client
+    LinkedIn.configure do |config|
+      config.token = ENV['LINKEDIN_KEY']
+      config.secret = ENV['LINKEDIN_SECRET']
+      config.default_profile_fields = ['certifications','educations',
+        'phone-numbers','positions','picture-url','skills','summary']
+    end
+    linked_in = LinkedIn::Client.new
+    linked_in_client ||= linked_in
+  end
+
 end
