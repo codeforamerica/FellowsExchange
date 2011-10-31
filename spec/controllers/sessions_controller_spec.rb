93d9f74759265206c9e456881ab75d1d3535c73b
Ryan Resella <ryan@codeforamerica.org>
2011-10-31 16:46:13 -0700

    Added spec for destroy session

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,13 +6,20 @@ describe SessionsController do
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:linked_in]
   end
 
-
-    describe "#create" do
-      it "should create a new user using omniauth" do
-        get :create, :provider => 'linked_in'
-        @user = User.last
-        @user.provider.should == "linked_in"
-      end
+  describe "#create" do
+    it "should create a new user using omniauth" do
+      get :create, :provider => 'linked_in'
+      @user = User.last
+      @user.provider.should == "linked_in"
     end
+  end
+
+  describe "GET 'destroy'" do
+    it "should destroy the session" do
+      get 'destroy'
+      session['user_id'].should be_nil
+      response.should redirect_to root_path
+    end
+  end
 
 end
