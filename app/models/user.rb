aa6a2813a65aad81caa591d6fe4561fbcb38438b
Ryan Resella <ryan@codeforamerica.org>
2011-10-31 14:57:00 -0700

    Added acts as taggable on skills and interests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
+
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
