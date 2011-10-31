4e22177b355f0b0e75daa69e62a22b4f14fcfdcd
Ryan Resella <ryan@codeforamerica.org>
2011-10-31 14:40:03 -0700

    Added users for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env != 'production'
+
+require 'faker'
+require 'factory_girl_rails'
+
+puts "adding 20 users"
+  20.times {
+    user = Factory(:user, :name => Faker::Name.first_name + ' ' + Faker::Name.last_name)
+    }
+
+end
