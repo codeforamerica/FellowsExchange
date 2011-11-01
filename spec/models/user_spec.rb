914d2dc076a0d15e990a2c4e980bc4c3edd5a4ce
Ryan Resella <ryan@codeforamerica.org>
2011-11-01 12:14:55 -0700

    Added spec for add skills info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @auth = {
+    'provider' => 'linked_in',
+    'uid' => '123545',
+    'user_info' => {'name' => "Ben Franklin"},
+    'credentials' => {'token' => 'abc123', 'secret' => 'xyz987'}
+    }
+  end
+
+  describe "#add_linked_in_info" do
+    before do
+      stub_request(:get, "https://api.linkedin.com/v1/people/~:(certifications,educations,phone-numbers,positions,picture-url,skills,summary)").
+        to_return(:status => 200, :body => fixture("linked_in_profile.json"), :headers => {})
+    end
+
+    it "should add the linked in info to the user" do
+      @client = User.new
+      test = @client.add_linked_in_info(@auth)
+      test.should == "Ruby on Rails,ESRI,GIS"
+    end
+  end
+
 end
