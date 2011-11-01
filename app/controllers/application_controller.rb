7091e006d74b679d0ddf0ba619a3c16e4411552b
Ryan Resella <ryan@codeforamerica.org>
2011-11-01 13:07:47 -0700

    Added pull from LinkedIn info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def client
+    LinkedIn.configure do |config|
+      config.token = ENV['LINKEDIN_KEY']
+      config.secret = ENV['LINKEDIN_SECRET']
+      config.default_profile_fields = ['id','first-name','last-name','headline','location',
+        'industry','distance','relation-to-viewer','current-share','connections','num-connections',
+        'num-connections-capped','summary','specialties','proposal-comments','associations','honors',
+        'interests','positions','publications','patents','languages','skills','certifications',
+        'educations','three-current-positions','three-past-positions','num-recommenders',
+        'recommendations-received','phone-numbers','im-accounts','twitter-accounts','date-of-birth',
+        'main-address','member-url-resources','picture-url','site-standard-profile-request',
+        'api-public-profile-request','site-public-profile-request','api-standard-profile-request',
+        'public-profile-url']
+    end
+    @client ||= LinkedIn::Client.new
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+    session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
     redirect_to root_url, :notice => "Signed in!"
   end
 
