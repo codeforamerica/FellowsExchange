395257c76f61b4da94080628603f8743cfef1a16
Ryan Resella <ryan@codeforamerica.org>
2011-11-04 17:27:44 -0700

    Added default scope for name on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :tags, :skills,:interests
 
+  default_scope order("name asc")
 
   def self.create_with_omniauth(auth)
     create! do |user|
